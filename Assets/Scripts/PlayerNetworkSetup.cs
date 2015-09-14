c79130fd7de098a61058c9a47df799ca2e59e73e
Ankit Kumar <me@kumarankit.com>
2015-09-14 11:04:45 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+    [SerializeField] Camera FPSCharacterCam;
+    [SerializeField]
+    AudioListener audioListener;
+	// Use this for initialization
+	void Start () {
+	if(isLocalPlayer)
+        {
+            GameObject.Find("Scene Camera").SetActive(false);
+            GetComponent<CharacterController>().enabled = true;
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+            FPSCharacterCam.enabled = true;
+            audioListener.enabled = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
