a867ba665b5079679052fa3385f1611ea95207b3
Ankit Kumar <me@kumarankit.com>
2015-09-14 11:20:40 -0700

    Initial commit

--- Assets/Scripts/PlayerSyncPosition.cs
+++ Assets/Scripts/PlayerSyncPosition.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class PlayerSyncPosition : MonoBehaviour {
+public class PlayerSyncPosition : NetworkBehaviour {
 
+    private Vector3 syncPos;
+    [SerializeField] Transform myTransform;
+    [SerializeField] float lerpRate = 15;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        transmitPosition();
+        lerpPosition();
+
 	}
+    void lerpPosition()
+    {
+        if(!isLocalPlayer)
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdProvidePositionToServer(Vector3 pos)
+    {
+        syncPos = pos;
+    }
+
+    [ClientCallback]
+    void transmitPosition()
+    {
+        if (isLocalPlayer)
+        {
+            CmdProvidePositionToServer(myTransform.position);
+        }
+    }
 }
